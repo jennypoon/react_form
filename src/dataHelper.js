878d8c906fedfbfab11db14d70a0cca2ac347996
Jenny Poon <jenny.poon@live.ca>
2019-01-27 13:50:05 -0800

    added file to store functions

---
+++ src/dataHelper.js
@@ -0,0 +1,11 @@
+function validateEmail(input) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(input).toLowerCase());
+}
+
+function validatePhone(input) {
+  var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  return re.test(input)
+}
+
+export default validateEmail
