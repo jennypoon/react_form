5b9fc703730cdf75d339e7dce47b6683e9da255a
Jenny Poon <jenny.poon@live.ca>
2019-01-27 13:49:41 -0800

    function for validations

--- src/components/ProfileOne.jsx
+++ src/components/ProfileOne.jsx
@@ -4,10 +4,13 @@ class FormOne extends Component {
     constructor() {
       super();
       this.state = {
-        // active: false
+          email: '',
+          phone: 0,
       };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    this.validateEmail = this.validateEmail.bind(this);
   }
 
   handleChange(event) {
@@ -17,6 +20,20 @@ class FormOne extends Component {
     console.log(this.state)
   }
 
+  handleBlur(event) {
+    if (this.validateEmail(event.target.value)) {
+      this.props.handleError("email", 0)
+    } else {
+      this.props.handleError("email", 1)
+    }
+  }
+
+  validateEmail(input) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(input).toLowerCase());
+  }
+
+
   render() {
     return (
      <div className="container">
@@ -24,16 +41,24 @@ class FormOne extends Component {
         <ul className="flex-outer">
           <li>
             <label htmlFor="first-name">First Name</label>
-            <input type="text" name="first-name" id="first-name" onChange={this.handleChange} placeholder="Enter your first name here" required/>
+            <input type="text" name="firstName" id="first-name"
+                    onChange={this.handleChange}
+                    placeholder="Enter your first name here"
+                    required/>
           </li>
           <li>
             <label htmlFor="last-name">Last Name</label>
-            <input type="text" name="last-name" id="last-name" onChange={this.handleChange} placeholder="Enter your last name here" required/>
+            <input type="text" name="lastName" id="last-name" onChange={this.handleChange} placeholder="Enter your last name here" required/>
           </li>
           <li>
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" onChange={this.handleChange} placeholder="Enter your email here" required/>
+            <input type="email" name="email" id="email"
+                    onChange={this.handleChange}
+                    onBlur={this.handleBlur}
+                    placeholder="Enter your email here" required/>
           </li>
+          <label  className={this.props.emailError === false ? "notError" : "error"} > Error - Please insert a valid email</label>
+
           <li>
             <label htmlFor="phone">Phone</label>
             <input type="tel" name="phone" id="phone" onChange={this.handleChange} placeholder="Enter your phone here" required/>
--- src/components/ProfileTwo.jsx
+++ src/components/ProfileTwo.jsx
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
 class FormTwo extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+
+  handleChange(event) {
+    event.preventDefault();
+    console.log(event.target.name)
+    this.setState({ [event.target.name]: event.target.value });
+  }
 
   render() {
     return (
@@ -8,26 +24,31 @@ class FormTwo extends Component {
       <form>
         <ul className="flex-outer">
           <li>
-            <label for="first-name">First Name</label>
-            <input type="text" id="first-name" placeholder="Enter your first name here"/>
+            <label htmlFor="first-name">First Name</label>
+            <input type="text" id="first-name" onChange={this.handleChange} placeholder="Enter your first name here"/>
           </li>
           <li>
-            <label for="last-name">Last Name</label>
-            <input type="text" id="last-name" placeholder="Enter your last name here"/>
+            <label htmlFor="last-name">Last Name</label>
+            <input type="text" id="last-name" onChange={this.handleChange} placeholder="Enter your last name here"/>
           </li>
           <li>
-            <label for="email">Email</label>
-            <input type="email" id="email" placeholder="Enter your email here"/>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" onChange={this.handleChange} placeholder="Enter your email here"/>
           </li>
           <li>
-            <label for="phone">Phone</label>
-            <input type="tel" id="phone" placeholder="Enter your phone here"/>
+            <label htmlFor="phone">Phone</label>
+            <input type="tel" id="phone" onChange={this.handleChange} placeholder="Enter your phone here"/>
           </li>
 
-            <label for="message">
+            <label htmlFor="question">
               <label> Question: </label>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?</p></label>
-            <textarea rows="6" id="message" placeholder="Enter your message here"></textarea>
+            <textarea rows="6" id="questionOne" onChange={this.handleChange} placeholder="Enter your answer here"></textarea>
+
+            <label htmlFor="question">
+              <label> Question: </label>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna alique?</p></label>
+            <textarea rows="5" id="questionTwo" onChange={this.handleChange} placeholder="Enter your answer here"></textarea>
         </ul>
             <button type="submit">Submit</button>
       </form>
