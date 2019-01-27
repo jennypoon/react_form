a0a402a6b2e723c35b0910db463db2f091aae2de
Jenny Poon <jenny.poon@live.ca>
2019-01-27 12:22:34 -0800

    added handleChange for form

--- src/components/ProfileOne.jsx
+++ src/components/ProfileOne.jsx
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 
 class FormOne extends Component {
+    constructor() {
+      super();
+      this.state = {
+        // active: false
+      };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    console.log(event.target.value)
+    event.preventDefault();
+    this.setState({ [event.target.name]: event.target.value });
+    console.log(this.state)
+  }
 
   render() {
     return (
@@ -8,20 +23,20 @@ class FormOne extends Component {
       <form>
         <ul className="flex-outer">
           <li>
-            <label for="first-name">First Name</label>
-            <input type="text" id="first-name" placeholder="Enter your first name here"/>
+            <label htmlFor="first-name">First Name</label>
+            <input type="text" name="first-name" id="first-name" onChange={this.handleChange} placeholder="Enter your first name here" required/>
           </li>
           <li>
-            <label for="last-name">Last Name</label>
-            <input type="text" id="last-name" placeholder="Enter your last name here"/>
+            <label htmlFor="last-name">Last Name</label>
+            <input type="text" name="last-name" id="last-name" onChange={this.handleChange} placeholder="Enter your last name here" required/>
           </li>
           <li>
-            <label for="email">Email</label>
-            <input type="email" id="email" placeholder="Enter your email here"/>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" onChange={this.handleChange} placeholder="Enter your email here" required/>
           </li>
           <li>
-            <label for="phone">Phone</label>
-            <input type="tel" id="phone" placeholder="Enter your phone here"/>
+            <label htmlFor="phone">Phone</label>
+            <input type="tel" name="phone" id="phone" onChange={this.handleChange} placeholder="Enter your phone here" required/>
           </li>
 
             <button type="submit">Submit</button>
