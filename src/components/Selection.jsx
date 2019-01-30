53ca4e85e000c4c624e5dc067bc58c3df8c074f8
Jenny Poon <jenny.poon@live.ca>
2019-01-29 23:22:05 -0800

    added checkbox inputs

--- src/components/Selection.jsx
+++ src/components/Selection.jsx
@@ -34,46 +34,53 @@ class Selection extends Component {
         const activeClass = this.state.active ? "active" : "inactive";
 
     return (
-      <div>
-        <div className={activeClass}>
-          <span className="header" onClick={this.toggle}>2. Select</span>
-          <span className="folding-panel answer">
-            <form className="rsvpForm" onSubmit={this.handleSubmit}>
-              <label className="rsvpColumn">
-                First Name:
-              </label>
-              <input className="rsvpColumn input" type="text" name="name" onChange={this.handleChange} required ></input><br/><br/>
+      <div className="container">
+      <form>
+        <ul className="flex-outer">
+          <li>
+            <h2>Checkboxes</h2>
+            <div className="inputGroup option">
+              <input id="option1" name="option1" type="checkbox"/>
+              <label for="option1">Option One</label>
+            </div>
 
-              <label className="rsvpColumn">
-                Last Name:
-              </label>
-              <input className="rsvpColumn input" type="text" name="name" onChange={this.handleChange} required ></input><br/><br/>
+            <div className="inputGroup option">
+              <input id="option2" name="option2" type="checkbox"/>
+              <label for="option2">Option Two</label>
+            </div>
 
-              <label className="rsvpColumn">
-                Your Email:
+            <div className="inputGroup">
+              <input id="option3" name="option3" value="700" type="checkbox"/>
+              <label for="option3">
+                <h3>Option Three</h3>
+                <li>DESCRIPTION: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?</li>
+                <li>COST: $700.00 </li>
               </label>
-              <input className="rsvpColumn input" type="text" name="email" onChange={this.handleChange} required ></input><br/><br/>
+             </div>
 
-              <label className="rsvpColumn">
-               Street Address
+            <div className="inputGroup">
+              <input id="option4" name="option4" value="500" type="checkbox"/>
+              <label for="option4">
+                  <h3>Option Four</h3>
+                  <li>DESCRIPTION: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco </li>
+                  <li>COST: $500.00 </li>
+             </label>
+            </div>
+
+            <div className="inputGroup">
+              <input id="option5" name="option5" value="350" type="checkbox"/>
+              <label for="option5">
+                  <h3>Option Five</h3>
+                  <li>DESCRIPTION: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam </li>
+                  <li>COST: $350.00 </li>
               </label>
-              <input className="rsvpColumn input" type="text" name="plusone" onChange={this.handleChange}></input><br/><br/>
+            </div>
 
-              <label className="rsvpColumn">
-               City
-              </label>
-              <input className="rsvpColumn input" type="text" name="guestNames" onChange={this.handleChange}></input><br/><br/>
-
-               <label className="rsvpColumn">
-                Province
-              </label>
-              <button className="rsvpButton" onClick={this.handleClick}> Review Your Order </button>
-            </form>
-      )}
-
-          </span>
-        </div>
-      </div>
+          </li>
+          </ul>
+            <button type="submit">Submit</button>
+      </form>
+    </div>
 
       )
   }
