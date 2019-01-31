2b2fe19fb4271197e76a78cc6def8a31735e800b
Jenny Poon <jenny.poon@live.ca>
2019-01-31 13:02:32 -0800

    prop to send final information back to App

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
 import Profile from './Profile.jsx';
 import Selection from './Selection.jsx';
-import Checkout from './Checkout.jsx';
+import Review from './Review.jsx';
 import { Accordion, AccordionItem } from 'react-light-accordion';
 
 class Form extends Component {
 
   render() {
+    console.log("FORM PROPS", this.props)
     return (
       <div className="form_container">
         <Accordion atomic={true}>
 
           <AccordionItem title="Profile">
-            <Profile />
+            <Profile profileState={this.props.profileState}/>
           </AccordionItem>
 
           <AccordionItem title="Selection">
-            <Selection quoteState={this.props.quoteState}/>
+            <Selection quoteState={this.props.quoteState}
+                       finalSelectionState={this.props.finalSelectionState}/>
           </AccordionItem>
 
           <AccordionItem title="Review">
-            <Checkout />
+            <Review profile={this.props.profile}
+                    finalSelection={this.props.finalSelection}/>
           </AccordionItem>
 
         </Accordion>
