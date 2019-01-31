18a9d9533236c3703751ea164c23397eaa304b0b
Jenny Poon <jenny.poon@live.ca>
2019-01-30 22:24:28 -0800

    individual checkboxes

---
+++ src/components/Checkbox.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+
+class Checkbox extends Component {
+  constructor() {
+    super()
+  this.state = {
+    isChecked: false,
+  }
+
+}
+  toggleCheckboxChange = (event) => {
+    let isChecked = this.state.isChecked
+    let name = event.target.name
+    let value = event.target.value
+    const { handleCheckboxChange, label } = this.props;
+
+    this.setState({
+      name: name,
+      value: value,
+      isChecked: !isChecked
+    }, () => {
+      if (isChecked === false) {
+        this.props.draftQuote(this.state.name, this.state.value)
+      } else {
+        this.props.draftQuote(this.state.name, 0)
+      }
+    });
+
+    handleCheckboxChange(label);
+
+  }
+
+  render() {
+    const { name } = this.props.label;
+    const { label } = this.props
+    const { isChecked } = this.state;
+    const item = name.charAt(0).toUpperCase() + name.slice(1).replace("_", " ")
+    return (
+        <label>
+          <input
+            id={label.key}
+            name={label.name}
+            type="checkbox"
+            value={label.value}
+            checked={isChecked}
+            onChange={this.toggleCheckboxChange}
+          />
+          <span>{item} - ${label.value}</span>
+          <p>{label.description}</p>
+        </label>
+    );
+  }
+}
+
+export default Checkbox;
