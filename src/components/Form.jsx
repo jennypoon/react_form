1aac4bb6da832fcb7c8a2338d768e63273746f3f
Jenny Poon <jenny.poon@live.ca>
2019-01-31 17:58:16 -0800

    added Draftquote component and associated functions to retrieve state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,30 +3,48 @@ import Profile from './Profile.jsx';
 import Selection from './Selection.jsx';
 import Review from './Review.jsx';
 import { Accordion, AccordionItem } from 'react-light-accordion';
+import DraftQuote from './Draftquote.jsx';
 
 class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+      testing: 0
+    }
+
+    this.quoteStates = this.quoteStates.bind(this)
+
+  }
+
+  quoteStates(item, value) {
+    this.setState({[item]: value})
+  }
 
   render() {
-    console.log("FORM PROPS", this.props)
     return (
-      <div className="form_container">
-        <Accordion atomic={true}>
+      <div>
+        <div>
+          <DraftQuote formState={this.state}/>
+        </div>
+        <div className="form_container">
+          <Accordion atomic={true}>
 
-          <AccordionItem title="Profile">
-            <Profile profileState={this.props.profileState}/>
-          </AccordionItem>
+            <AccordionItem title="Profile">
+              <Profile profileState={this.props.profileState}/>
+            </AccordionItem>
 
-          <AccordionItem title="Selection">
-            <Selection quoteState={this.props.quoteState}
-                       finalSelectionState={this.props.finalSelectionState}/>
-          </AccordionItem>
+            <AccordionItem title="Selection">
+              <Selection quoteState={this.quoteStates}
+                         finalSelectionState={this.props.finalSelectionState}/>
+            </AccordionItem>
 
-          <AccordionItem title="Review">
-            <Review profile={this.props.profile}
-                    finalSelection={this.props.finalSelection}/>
-          </AccordionItem>
+            <AccordionItem title="Review">
+              <Review profile={this.props.profile}
+                      finalSelection={this.props.finalSelection}/>
+            </AccordionItem>
 
-        </Accordion>
+          </Accordion>
+        </div>
       </div>
     )
   }
