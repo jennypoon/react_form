c9c422647f60eb610bd34d488bb44cb8f8ddc64b
Jenny Poon <jenny.poon@live.ca>
2019-01-31 22:24:17 -0800

    cascading accordion

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Profile from './Profile.jsx';
 import Selection from './Selection.jsx';
 import Review from './Review.jsx';
-import { Accordion, AccordionItem } from 'react-light-accordion';
+// import { Accordion, AccordionItem } from 'react-light-accordion';
 import DraftQuote from './Draftquote.jsx';
 
 class Form extends Component {
@@ -21,6 +21,10 @@ class Form extends Component {
   }
 
   render() {
+    const { activeSection } = this.props
+    console.log("FORM - ACTION SECTION", activeSection)
+    const activeClass = "accordion-item active";
+
     return (
       <div>
 
@@ -30,29 +34,53 @@ class Form extends Component {
 
         <div className="form_container">
           <div className="accordion">
-            <div className="accordion-item active">
+            {(activeSection === 1) ?
+            (<div className="accordion-item active">
               <button className="title"> Profile </button>
               <div className="panel">
                 <Profile profileState={this.props.profileState}/>
               </div>
-            </div>
+            </div>) :
+            (<div className="accordion-item">
+              <button className="title"> Profile </button>
+              <div className="panel">
+                <Profile profileState={this.props.profileState}/>
+              </div>
+            </div>)}
 
-            <div className="accordion-item active">
+            {(activeSection === 2) ?
+            (<div className="accordion-item active">
+              <button className="title"> Order </button>
+              <div className="panel">
+               <Selection quoteState={this.quoteStates}
+                           finalSelectionState={this.props.finalSelectionState}/>
+              </div>
+            </div>) :
+            (<div className="accordion-item">
               <button className="title"> Order </button>
               <div className="panel">
                 <Selection quoteState={this.quoteStates}
                            finalSelectionState={this.props.finalSelectionState}/>
               </div>
-            </div>
+            </div>)}
 
-            <div className="accordion-item active">
+         {(activeSection === 3) ?
+            (<div className="accordion-item active">
+              <button className="title"> Review </button>
+              <div className="panel">
+               <Review profile={this.props.profile}
+                      finalSelection={this.props.finalSelection}/>
+              </div>
+            </div>) :
+            (<div className="accordion-item">
               <button className="title"> Review </button>
               <div className="panel">
                 <Review profile={this.props.profile}
                       finalSelection={this.props.finalSelection}/>
               </div>
-            </div>
-          </div>
+            </div>)}
+
+       </div>
       </div>
     </div>
     )
