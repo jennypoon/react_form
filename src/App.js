9a0d0751d085eb429c9bbd9e94b93aae6dad1891
Jenny Poon <jenny.poon@live.ca>
2019-01-31 17:57:32 -0800

    removed draftform and placed into Form component

--- src/App.js
+++ src/App.js
@@ -3,48 +3,39 @@ import './App.scss';
 import Form from './components/Form.jsx'
 import Tracker from './components/Tracker.jsx'
 import Summary from './components/Summary.jsx'
-import DraftQuote from './components/Draftquote.jsx'
+// import DraftQuote from './components/Draftquote.jsx'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      name: 0,
       activeSection: 1,
       profileInfo: null,
       finalSelection: null
     }
-    this.handleCheckedBox = this.handleCheckedBox.bind(this)
-    this.quoteStates = this.quoteStates.bind(this)
+
     this.profileState = this.profileState.bind(this)
     this.finalSelection = this.finalSelection.bind(this)
   }
 
-  quoteStates(item, value) {
-    this.setState({ [item]: value})
-  }
 
+//ON SUBMIT OF SELECTION COMPONENT
   finalSelection(quote) {
     console.log("FINAL SELECTION IN APP.jS", quote)
     this.setState({finalSelection: quote, activeSection: 3})
   }
 
+//ON SUBMIT OF PROFILE COMPONENT
   profileState(profile) {
     console.log("PROFILE IN APP.JS", profile)
     this.setState({profileInfo: profile, activeSection: 2})
   }
 
-  handleCheckedBox(item, value) {
-    this.setState({ [item]: value})
-  }
-
   render() {
-    console.log("STATE IN APP.JS", this.state)
     return (
       <div>
-        <DraftQuote formState={this.state} handleCheckedBox={this.handleCheckedBox}/>
         <Tracker />
-        <Form quoteState={this.quoteStates}
+        <Form
               profileState={this.profileState}
               finalSelectionState={this.finalSelection}
               activeSection={this.state.activeSection}
