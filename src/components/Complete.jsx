30f90c8a0b26f0c09257f930f5b42dc33308b7ed
Jenny Poon <jenny.poon@live.ca>
2019-02-01 17:58:12 -0800

    fixed typo

--- src/components/Complete.jsx
+++ src/components/Complete.jsx
@@ -16,7 +16,7 @@ class Tracker extends Component {
     return (
       <div className="form_container">
         <div className="completion">
-          <p>Thank you! You're request is currently being processed</p>
+          <p>Thank you! Your request is currently being processed</p>
           <button name="profile" type="submit" onClick={this.handleEdit}>Start Over</button>
         </div>
 </div>
