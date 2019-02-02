a8005d4144927f3f09aae5ce1e0a19ff61bd482a
Jenny Poon <jenny.poon@live.ca>
2019-02-01 17:02:12 -0800

    handle completion message

---
+++ src/components/Complete.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Tracker extends Component {
+  constructor() {
+    super()
+
+    this.handleEdit = this.handleEdit.bind(this)
+  }
+
+  handleEdit(event) {
+    this.props.handleRedirect(1)
+    this.props.updateCompletion(false)
+  }
+
+  render() {
+    return (
+      <div className="form_container">
+        <div className="completion">
+          <p>Thank you! You're request is currently being processed</p>
+          <button name="profile" type="submit" onClick={this.handleEdit}>Start Over</button>
+        </div>
+</div>
+      )
+  }
+}
+
+export default Tracker;
