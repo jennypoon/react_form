e748b8966e02f63d7fe693a31971c43f9debec8a
Jenny Poon <jenny.poon@live.ca>
2019-04-20 21:09:35 -0700

    WIP

--- src/dataHelper.js
+++ src/dataHelper.js
@@ -1,13 +1,12 @@
-function validateEmail(input) {
+const validateEmail = (input) => {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(input).toLowerCase());
+  return re.test(String(input).toLowerCase().trim());
 }
 
-function validatePhone(input) {
+const validatePhone = (input) => {
   var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-  return re.test(input)
+  return re.test(input.trim())
 }
-
 
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -4,7 +4,29 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+
+const activePage = (state = 1, action) => {
+  switch(action.type) {
+    case 'SET_NEXT_PAGE':
+     return state = state + 1
+    case 'SET_PREV_PAGE':
+      return state = state - 1
+    default:
+      return state
+  }
+}
+
+
+const store = createStore(activePage);
+
+
+ReactDOM.render(
+ <Provider store={store}>
+   <App />
+ </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
